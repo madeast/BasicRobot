924bb24a8d08f112c154896a05f5d640917457bd
Easton Madsen <eeagle25@comcast.net>
2015-12-16 08:48:13 -0700

    Made robot Project

---
+++ src/bot/controller/EastonBotRunner.java
@@ -0,0 +1,13 @@
+package bot.controller;
+
+public class EastonBotRunner
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		BotController myBotController = new BotController();
+		myBotController.start();
+	}
+
+}
